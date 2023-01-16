a47b29bab7f865a2eb4b060d5696aa86f964879a
uktherebel <55969501+uktherebel@users.noreply.github.com>
2023-01-16 08:49:12 +0500

    Delete COVID-19 Data Exploration directory

--- COVID-19 Data Exploration/Part 1 SQL Data Exploration/SQLQuery.sql
+++
@@ -1,7 +0,0 @@
-SELECT dea.continent, dea.location, dea.date, dea.population, vac.new_vaccinations, sum(vac.new_vaccinations) OVER (PARTITION BY dea.LOCATION)
-FROM COVID_Project..COVID_DEATHS dea
-JOIN COVID_Project..COVID_Vaccinations vac
-    ON dea.[location] = vac.[location]
-    and dea.[date] = vac.[date]
-WHERE dea.continent IS NOT null 
-Order by 2,3
